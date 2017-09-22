a85a12b7001e6398c50b2a634450674c248ba2fc
Nick Lee <severejetlag@Nicks-MacBook-Pro.local>
2017-09-21 23:57:19 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "king";
+var cardTwo = "quen";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User has flipped " + cardOne);
+console.log("User has flipped " + cardThree);
