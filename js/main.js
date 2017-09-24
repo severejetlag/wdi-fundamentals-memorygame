577c00e885f31edb34c247021dc3216104d473d9
severejetlag <nhl1013@gmail.com>
2017-09-24 16:32:08 -0700

    made game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running!");
 
-var cardOne = "king";
-var cardTwo = "quen";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User has flipped " + cardOne);
-console.log("User has flipped " + cardThree);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You've found a match!");
+	}else{
+		alert("Sorry...try again");
+	}
+}
